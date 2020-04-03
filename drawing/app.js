d163d011f0b530d21092833bd0d6ec13bdf05385
Eszti <koveshazieszter@gmail.com>
2020-04-03 18:52:18 +0200

    add line

--- drawing/app.js
+++ drawing/app.js
@@ -1,5 +1,8 @@
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
 
-context.fillStyle = 'orange';
-context.fillRect(125, 50, 200, 200);
+context.beginPath();
+context.moveTo(0, 300);
+context.lineTo(450, 0);
+context.strokeStyle = 'red';
+context.stroke();
