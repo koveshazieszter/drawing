e9e03855ba95c058df652deb4d7736ff43e361e0
koveshazieszter <45720914+koveshazieszter@users.noreply.github.com>
2020-04-03 15:45:32 +0200

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-var canvas = document.getElementById('myCanvas');
-var context = canvas.getContext('2d');
-context.fillRect(175,100,100,100);
-context.fillStyle = 'red';
-context.fillRect(20,10,250,175);
